4c9249d2439bee3a8782cfdeccbd9a6abcf15b08
Олександра <aleksa12285@gmail.com>
2025-04-14 15:17:30 +0300

    task 2

---
+++ Task_2.py
@@ -0,0 +1,20 @@
+def get_cats_info(path):
+    try:
+        cats_list = []
+        with open(path, 'r', encoding='utf-8') as file:
+            for line in file:
+                parts = line.strip().split(',')
+                if len(parts) == 3:
+                    cat_id, name, age = parts
+                    cats_list.append({
+                        "id": cat_id,
+                        "name": name,
+                        "age": age
+                    })
+        return cats_list
+    except FileNotFoundError:
+        print(f"Файл за шляхом {path} не знайдено.")
+        return []
+    except Exception as e:
+        print(f"Сталася помилка при читанні файлу: {e}")
+        return []
